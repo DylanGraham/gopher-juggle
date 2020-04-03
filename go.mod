fba1f62fd3b18cb4320edb5878a8190ca376c196
Dylan Graham <dylan.graham@gmail.com>
2020-04-03 23:06:09 +1100

    hello Ebiten

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dylangraham/gopher-juggle
+
+go 1.14
+
+require github.com/hajimehoshi/ebiten v1.10.5
--- main.go
+++ main.go
@@ -3,35 +3,20 @@ package main
 import (
 	"log"
 
-	"fyne.io/fyne"
-	"fyne.io/fyne/app"
-	"fyne.io/fyne/canvas"
-	"fyne.io/fyne/layout"
-	"fyne.io/fyne/widget"
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
-func main() {
-	myApp := app.New()
-	myWindow := myApp.NewWindow("Gopher Juggle")
-
-	gopherIcon1 := canvas.NewImageFromFile("buff.png")
-	content := widget.NewButton("click me", func() {
-		log.Println("tapped")
-	})
-
-	container := fyne.NewContainerWithLayout(layout.NewMaxLayout(), content, gopherIcon1)
-
-	// myWidget := widget.NewButtonWithIcon("", gopherIcon1, func() {
-	// 	log.Println("Clicked!")
-	// })
-
-	// go changeContent(myCanvas)
-
-	myWindow.Resize(fyne.NewSize(500, 500))
-	myWindow.SetContent(container)
-	myWindow.ShowAndRun()
+func update(screen *ebiten.Image) error {
+	if ebiten.IsDrawingSkipped() {
+		return nil
+	}
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+	return nil
 }
 
-func changeContent(c fyne.Canvas) {
-
+func main() {
+	if err := ebiten.Run(update, 320, 240, 2, "Hello, World!"); err != nil {
+		log.Fatal(err)
+	}
 }
