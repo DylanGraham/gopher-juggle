a934e011e68a069f025da05f3765c80d62a9517e
Dylan Graham <dylan.graham@gmail.com>
2020-04-07 18:56:25 +1000

    clean

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/dylangraham/gopher-juggle
 
 go 1.14
 
-require github.com/hajimehoshi/ebiten v1.10.5
+require github.com/hajimehoshi/ebiten v1.11.0-rc.2
--- main.go
+++ main.go
@@ -1,22 +1,64 @@
 package main
 
 import (
+	_ "image/png"
 	"log"
 
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
-func update(screen *ebiten.Image) error {
-	if ebiten.IsDrawingSkipped() {
-		return nil
+const (
+	width  = 500
+	height = 500
+)
+
+var (
+	gopher1 *ebiten.Image
+	gopher2 *ebiten.Image
+	gopher3 *ebiten.Image
+)
+
+func init() {
+	gopher1, _, err := ebitenutil.NewImageFromFile("ball.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
 	}
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	gopher2, _, err := ebitenutil.NewImageFromFile("ball.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+	gopher3, _, err := ebitenutil.NewImageFromFile("ball.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	println(gopher1)
+	println(gopher2)
+	println(gopher3)
+}
+
+type Game struct{}
+
+func (g *Game) Update(screen *ebiten.Image) error {
+	// Update the game state
 	return nil
 }
 
+func (g *Game) Draw(screen *ebiten.Image) {
+	// Draw the current game state
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	// Accepts the outside size (e.g., window size), and
+	// returns the game screen size.
+	// The game screen scale is automatically adjusted.
+	return 320, 240
+}
+
 func main() {
-	if err := ebiten.Run(update, 320, 240, 2, "Hello, World!"); err != nil {
-		log.Fatal(err)
+	g := &Game{}
+	if err := ebiten.RunGame(g); err != nil {
+		panic(err)
 	}
 }
